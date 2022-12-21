1c461a83227320639432c71266f5d6983e721572
Michael Diekmann <michaeldiekmann@gmail.com>
2022-12-20 17:25:48 -0800

    Update ColorResourceExt.kt

--- toolkit/src/iosMain/kotlin/com/mtd/toolkit/ColorResourceExt.kt
+++ toolkit/src/iosMain/kotlin/com/mtd/toolkit/ColorResourceExt.kt
@@ -3,7 +3,7 @@ package com.mtd.toolkit
 import platform.UIKit.UIColor
 import dev.icerock.moko.resources.ColorResource
 
-actual typealias PlatformColor: UIColor
+actual typealias PlatformColor = UIColor
 
 actual fun ColorResource.Single.toPlatformColor(): PlatformColor {
     return UIColor.blueColor
