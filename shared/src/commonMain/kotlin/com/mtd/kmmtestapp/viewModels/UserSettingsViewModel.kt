a5c397716d25f260d8e85c9109bb252ff746482e
mDiekmann <michaeldiekmann@gmail.com>
2023-10-29 18:08:01 -0700

    Update UserSettingsViewModel.kt

--- shared/src/commonMain/kotlin/com/mtd/kmmtestapp/viewModels/UserSettingsViewModel.kt
+++ shared/src/commonMain/kotlin/com/mtd/kmmtestapp/viewModels/UserSettingsViewModel.kt
@@ -2,21 +2,24 @@ package com.mtd.kmmtestapp.viewModels
 
 import co.touchlab.kermit.Logger
 import com.mtd.kmmtestapp.UserSettings
-import com.rickclephas.kmm.viewmodel.KMMViewModel
-import com.rickclephas.kmm.viewmodel.MutableStateFlow
-import com.rickclephas.kmp.nativecoroutines.NativeCoroutinesState
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 
-open class UserSettingsViewModel() : KMMViewModel(), KoinComponent {
+open class UserSettingsViewModel() : ViewModel(), KoinComponent {
     private val userSettings : UserSettings by inject()
     private val logger = Logger.withTag("UserSettingsViewModel")
 
-    private val _roomSlug = MutableStateFlow<String?>(viewModelScope, userSettings.getRoomSlug())
-
-    @NativeCoroutinesState
+    private val _roomSlug = MutableStateFlow<String?>(userSettings.getRoomSlug())
     val roomSlug = _roomSlug.asStateFlow()
+
+    suspend fun activate() {
+    }
+
+    override fun onCleared() {
+        logger.v("Clearing UserSettingsViewModel")
+    }
 
     fun setRoomSlug(roomSlug: String?) {
         logger.d("Setting room slug to $roomSlug")
