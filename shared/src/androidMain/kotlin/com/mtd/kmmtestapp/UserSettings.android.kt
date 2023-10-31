3b18dd1c463be044125428fe4e1c20168308d540
Michael Diekmann <michael.diekmann@bounteous.com>
2023-10-30 17:58:09 -0700

    Code cleanup
    Spacing, move roomSlugKey into UserSettings object, remove factory implementation since it is injected

--- shared/src/androidMain/kotlin/com/mtd/kmmtestapp/UserSettings.android.kt
+++ shared/src/androidMain/kotlin/com/mtd/kmmtestapp/UserSettings.android.kt
@@ -6,16 +6,6 @@ import android.content.SharedPreferences
 public class AndroidUserSettings public constructor(
     private val delegate: SharedPreferences
 ) : UserSettings {
-    public class Factory(context: Context) : UserSettings.Factory {
-        private val appContext = context.applicationContext
-
-        public override fun create(): AndroidUserSettings {
-            val preferencesName = "${appContext.packageName}_preferences"
-            val delegate = appContext.getSharedPreferences(preferencesName, MODE_PRIVATE)
-            return AndroidUserSettings(delegate)
-        }
-    }
-
     public override fun getRoomSlug(): String? = delegate.getString(roomSlugKey, null)
     public override fun setRoomSlug(roomSlug: String?) = delegate.edit().putString(roomSlugKey, roomSlug).apply()
 }
